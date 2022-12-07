3a19cd18ddfa1ce3a1aa64326561d72ac0f919ac
Gian Souza <gian@exemplo.com>
2022-12-06 21:04:18 -0300

    corrigindo a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 2, 2022 19:00:00");
+const dataDoEvento = new Date("Dec 12, 2022 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
